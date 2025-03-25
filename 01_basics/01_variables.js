16e9cdf467304dd9d638fa8cd621988ab1e94650
Reetu Gupta <gupta.mukn2001@gmail.com>
2025-03-25 08:00:30 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 1111; // can not be redefined
+let accountEmail = "reetu@gmail.com";
+var accountPassword = "1234"; // not to use bcz of scope issue
+accountCity = "Lucknow";
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
