6a4a491e5c9e25824ee50d531bd29db79fc0afae
Reetu Gupta <gupta.mukn2001@gmail.com>
2025-03-26 12:15:16 +0000

    strins done

---
+++ 01_basics/5_string.js
@@ -0,0 +1,23 @@
+const name = "ritu"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+// old method in string
+
+//using bac ticks as new method 
+//console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`);
+
+//strings can also be defined using new keyword
+
+const gameName = new String('My Mario Game');
+//console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('G'));
+
+//there are various string methods like length, slice, substring, replace, trim ,etc
