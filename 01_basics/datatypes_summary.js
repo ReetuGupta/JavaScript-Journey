5c80b6966392552bb135446d4a4cd1dbc5901b3a
Reetu Gupta <gupta.mukn2001@gmail.com>
2025-03-26 09:22:54 +0000

    datatypes done

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -11,7 +11,7 @@
 
 //array
 const heros = ["shaktiman", "naagraj", "doga"];
-console.log( typeof heros);//object
+//console.log( typeof heros);//object
 
 //object
 let myObj = {
@@ -19,14 +19,59 @@ let myObj = {
     age: 22
 }
 
-console.log( typeof myObj); // object
+//console.log( typeof myObj); // object
 
 
 //function
 
 const myFunction = function(){
-    console.log("Hello world");
+   // console.log("Hello world");
 }
 
-console.log( typeof myFunction); // function or we say object function
+//console.log( typeof myFunction); // function or we say object function
+
+
+
+//************Stack and Heap*************
+
+//Stack(primitive) => for all primitive types stack memory is allocated means copy of the variable is created and if any changes is done then they will not reflect in original address 
+
+let tutorial = "jstutorial";
+
+let practice = tutorial;
+
+//console.log(practice); // jstutorial
+
+practice = "react";
+
+//console.log(practice);//react
+//console.log(tutorial);//jstutorial
+
+//bcz copy of tutorial is given to practice not the original
+
+
+
+
+//heap => all non primitive datatypes are stored in heap memory means reference of address is assigned no copy is created works on actual address so if any changes is done by variable then it will be reflected to original value.
+
+let js = {
+    frameworkone : "react",
+    frameworktwo : "vue",
+    frameworkthree : "angular"
+};
+
+let javaScript = js;
+
+console.log(javaScript.frameworkone);//react
+
+javaScript.frameworkone = "react.js";
+
+console.log(javaScript.frameworkone);//react.js
+
+console.log(js.frameworkone);//react.js
+
+console.log(js);
+
+//here actual js is changed from react to react.js
+
 
