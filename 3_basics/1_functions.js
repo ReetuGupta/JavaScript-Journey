4875ebed926b309aec3e9345305d4b7fe31d6df4
Reetu Gupta <gupta.mukn2001@gmail.com>
2025-03-29 07:57:05 +0000

    functions

---
+++ 3_basics/1_functions.js
@@ -0,0 +1,20 @@
+//function definition
+function myName() {
+    console.log("R");
+    console.log("E");
+    console.log("E");
+    console.log("T");
+    console.log("U");
+}
+
+//function reference
+myName //=> it will not print anything
+
+//function execution
+//myName()
+
+function add(num1, num2){//=> function parameters
+    console.log(num1 + num2);
+}
+
+add(5, 9)//=>function arguments
