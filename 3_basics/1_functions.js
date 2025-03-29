55c2c56a1e8a898f8f4a8df91faf85f31a76a315
Reetu Gupta <gupta.mukn2001@gmail.com>
2025-03-29 08:38:33 +0000

    function

--- 3_basics/1_functions.js
+++ 3_basics/1_functions.js
@@ -17,4 +17,15 @@ function add(num1, num2){//=> function parameters
     console.log(num1 + num2);
 }
 
-add(5, 9)//=>function arguments
+//add(5, 9)//=>function arguments
+
+//other ways
+
+function addnum(num1, num2){
+    // let result = num1 + num2;
+    // return result;
+    return num1 + num2
+}
+
+const result = addnum(5, 9)
+console.log("Result: ", result);
