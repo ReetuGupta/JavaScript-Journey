7648ea174b16ffcbd76901ad497bc98d5d667043
Reetu Gupta <gupta.mukn2001@gmail.com>
2025-03-30 03:37:24 +0000

    scope done

--- 3_basics/2_scope.js
+++ 3_basics/2_scope.js
@@ -8,8 +8,52 @@ if (true) { // function scope
     
 }
 
-
-
 // console.log(a);//300
 // console.log(b);
 // console.log(c); // 30 => which is wrong bcz c should not come out of block so not to use var
+
+
+function one(){
+    const username = "reetu"
+
+    function two(){
+        const website = "youtube"
+        console.log(username); // this will execute bcz its in one()
+    }
+    // console.log(website); // this will not bcz its not in scope bcz website is in two()
+
+    two()
+
+}
+
+//one() // reetu
+//if "//one()" then nothing will be displayed 
+
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website); => error
+}
+
+// console.log(username); =>error
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5)) //=> 6
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5) // error(Cannot access before initialization) bcz it can't be used like this
+const addTwo = function(num){
+    return num + 2
+}
